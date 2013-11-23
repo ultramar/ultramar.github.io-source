36b4320d67da7328824f9e364f91ac6500a64e5b
Henrique N <ultramar@hush.com>
2013-11-23 17:36:18 +0000

    Fix feeds url

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,7 +22,6 @@ DEFAULT_LANG = u'pt'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
 
 DEFAULT_PAGINATION = 5
 
